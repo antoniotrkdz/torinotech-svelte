9d74dc319ec070d7181723c8830dfe3efe9d83c8
antoniotrkdz <antoniotrkdz@gmail.com>
2020-03-06 03:41:42 +0100

    First commit

---
+++ src/components/Card.svelte
@@ -0,0 +1,35 @@
+<script>
+  export let title;
+  export let img;
+  export let description;
+  export let url;
+  export let contacts;
+  export let slug;
+</script>
+
+<div class="card">
+  <div class="card-image">
+    <figure class="image is-4by3">
+      <img src={"img/groups/"}{img} alt="Placeholder image">
+    </figure>
+  </div>
+  <div class="card-content">
+    <p class="title is-5">{title}</p>
+    <div class="content">
+      {description}
+
+      <ul>
+        <li><a href={url} target="_blank">Sito</a></li>
+        {#if contacts}
+          {#each contacts as contact}
+            <li><a href={contact.url} target="_blank">{contact.label}</a></li>
+          {/each}
+        {/if}
+      </ul>
+
+    </div>
+  </div>
+  <footer class="card-footer">
+    <a href={"groups/"}{slug} class="card-footer-item">Eventi</a>
+  </footer>
+</div>
---
+++ src/routes/index.svelte
@@ -0,0 +1,61 @@
+<script>
+  import Card from "../components/Card.svelte"
+  import groups from "../../data/groups.json"
+</script>
+
+<style>
+	h1, figure, p {
+		text-align: center;
+		margin: 0 auto;
+	}
+
+	h1 {
+		font-size: 2.8em;
+		text-transform: uppercase;
+		font-weight: 700;
+		margin: 0 0 0.5em 0;
+	}
+
+	figure {
+		margin: 0 0 1em 0;
+	}
+
+	img {
+		width: 100%;
+		max-width: 400px;
+		margin: 0 0 1em 0;
+	}
+
+	p {
+		margin: 1em auto;
+	}
+
+	@media (min-width: 480px) {
+		h1 {
+			font-size: 4em;
+		}
+	}
+</style>
+
+<svelte:head>
+	<title>Sapper project template</title>
+</svelte:head>
+
+<div class="section">
+  <div class="container">
+    <h1 class="title"><a href="/">Torino Tech</a></h1>
+  </div>
+</div>
+
+<section class="section">
+  <div class="container">
+    <b>Groups</b>
+      <div class="columns is-multiline">
+        {#each groups as group}
+        <div class="column is-one-quarter">
+          <Card {...group} />
+        </div>
+      {/each}
+    </div>
+  </div>
+</section>
